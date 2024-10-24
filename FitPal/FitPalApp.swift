5374e2c983b981bd2ba116e7bca0c828325305ce
Lavan Nithi <lavannithi2@gmail.com>
2024-10-23 20:54:03 -0400

    Initial Commit

---
+++ FitPal/FitPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPalApp.swift
+//  FitPal
+//
+//  Created by Lavan Nithi on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct FitPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitPalTests/FitPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitPalTests.swift
+//  FitPalTests
+//
+//  Created by Lavan Nithi on 2024-10-23.
+//
+
+import Testing
+@testable import FitPal
+
+struct FitPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
