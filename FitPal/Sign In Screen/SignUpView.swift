06a0be5749749acf1c51c892e14e826d3b457a16
Lavan Nithi <lavannithi2@gmail.com>
2024-11-28 11:54:53 -0500

    Email and Password validation

--- FitPal/Sign In Screen/SignUpView.swift
+++ FitPal/Sign In Screen/SignUpView.swift
@@ -13,6 +13,26 @@ final class SignUpViewModel: ObservableObject {
     @Published var password: String = ""
     
     func signUp(appState: AppState) async{
+        // Check if the password is at least 6 characters long
+        guard password.count >= 6 else {
+                print("Password must be at least 6 characters long.")
+                return
+        }
+        // Check if the email is valid using a regex
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        guard emailPredicate.evaluate(with: email) else {
+            print("Invalid email format.")
+            return
+        }
+        // Check if the password contains at least one letter, one number, and one special character
+        let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{6,15}$"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        guard passwordPredicate.evaluate(with: password) else {
+            print("Password must contain at least one letter, one number, and one special character.")
+            return
+        }
+        
         Task {
             do{
                 let returnedUserData = try await AuthenticationManager.shared.createUser(email: email, password: password)
