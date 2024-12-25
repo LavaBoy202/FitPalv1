228f9ead25be23961e8c1570fad226607f85ed43
Lavan Nithi <lavannithi2@gmail.com>
2024-12-25 07:54:55 -0500

    Display name in Profile View

--- FitPal/FitPalApp.swift
+++ FitPal/FitPalApp.swift
@@ -22,7 +22,8 @@ struct ContentView: View {
     @EnvironmentObject var appState: AppState
     var body: some View {
         if appState.isAuthenticated {
-            ProfileView()
+            MainView()
+                .environmentObject(appState)
         }else{
             TabView {
                 SignUpView()
